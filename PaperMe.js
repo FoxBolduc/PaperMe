815fc1bd3265f653c632b329fc87d341c300ae0e
Mitharlic <fox.bolduc@ttu.edu>
2017-08-07 17:37:29 -0600

    Added function to autofill resolution

---
+++ PaperMe.js
@@ -0,0 +1,9 @@
+//Sets default value for resolution to native
+$(document).ready(function(){
+    var width = screen.width;
+    var height = screen.height;
+    document.getElementById('width').value = width;
+    document.getElementById('height').value = height;
+    return;
+});
+
